d12d5902d6ea3138459db8a679b84265e27626d6
Antonio Pires <tonan@outlook.com.br>
2025-05-09 14:53:22 -0300

    13.2. Dealing with errors in a consumer using retry pattern

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -27,7 +27,7 @@ public class RabbitMQListener {
     @RabbitListener(queues = QUEUE_PROCESS_TEMPERATURE, concurrency = "2-3")
     public void handleProcessingTemperature(@Payload TemperatureLogData temperatureLogData) {
         temperatureMonitoringService.processTemperatureReading(temperatureLogData);
-        Thread.sleep(Duration.ofSeconds(5));
+        // Thread.sleep(Duration.ofSeconds(5));
     }
 
     @SneakyThrows
